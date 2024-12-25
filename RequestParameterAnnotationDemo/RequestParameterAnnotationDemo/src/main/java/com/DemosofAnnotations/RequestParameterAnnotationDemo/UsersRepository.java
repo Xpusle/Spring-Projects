41dff75b747d8e7190c2370ab6eedcc845a6a049
Siddarth <siddarthkanamdi915@gmail.com>
2024-12-25 14:40:41 +0530

    sidd

--- RequestParameterAnnotationDemo/RequestParameterAnnotationDemo/src/main/java/com/DemosofAnnotations/RequestParameterAnnotationDemo/UsersRepository.java
+++
@@ -1,7 +0,0 @@
-package com.DemosofAnnotations.RequestParameterAnnotationDemo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsersRepository extends JpaRepository<Users,Integer>{
-
-}
